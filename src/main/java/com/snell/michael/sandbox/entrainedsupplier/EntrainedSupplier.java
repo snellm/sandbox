02e3f1aad95058c73d18914dbc13247895446c3c
Michael Snell <michael@snell.com>
2015-05-17 14:57:00 +0100

    Testing EntrainedSupplier

--- src/main/java/com/snell/michael/sandbox/entrainedsupplier/EntrainedSupplier.java
+++ src/main/java/com/snell/michael/sandbox/entrainedsupplier/EntrainedSupplier.java
@@ -1,9 +1,14 @@
 package com.snell.michael.sandbox.entrainedsupplier;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.function.Supplier;
 
 public class EntrainedSupplier<T> implements Supplier<T> {
     private final Supplier<T> supplier;
+
+    private boolean running = false;
+    private CountDownLatch latch = null;
+    private T value = null;
 
     public EntrainedSupplier(Supplier<T> supplier) {
         this.supplier = supplier;
@@ -11,6 +16,26 @@ public class EntrainedSupplier<T> implements Supplier<T> {
 
     @Override
     public T get() {
-        return supplier.get();
+        boolean thisThreadWillRun = false;
+        synchronized (this) {
+            if (!running) {
+                running = true;
+                thisThreadWillRun = true;
+                latch = new CountDownLatch(1);
+            }
+        }
+
+        if (thisThreadWillRun) {
+            value = supplier.get();
+            latch.countDown();
+        } else {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Interrupted", e);
+            }
+        }
+
+        return value;
     }
 }
--- src/test/java/com/snell/michael/sandbox/entrainedsupplier/EntrainedSupplierTest.java
+++ src/test/java/com/snell/michael/sandbox/entrainedsupplier/EntrainedSupplierTest.java
@@ -2,13 +2,55 @@ package com.snell.michael.sandbox.entrainedsupplier;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.Assert.assertEquals;
 
 public class EntrainedSupplierTest {
+    private  static final String EXPECTED_RESULT = "Hello world";
+
     @Test
     public void callSupplier() {
-        EntrainedSupplier<String> supplier = new EntrainedSupplier<>(() -> "Hello world");
+        EntrainedSupplier<String> supplier = new EntrainedSupplier<>(() -> EXPECTED_RESULT);
 
-        assertEquals("Hello world", supplier.get());
+        assertEquals(EXPECTED_RESULT, supplier.get());
+    }
+
+    @Test
+    public void nestedSupplierCalledOnce() {
+        CountDownLatch releaseValueLatch = new CountDownLatch(1);
+        CountDownLatch threadsCompleteLatch = new CountDownLatch(2);
+        AtomicInteger valueCalculated = new AtomicInteger(0);
+        EntrainedSupplier<String> supplier = new EntrainedSupplier<>(() -> {
+            await(releaseValueLatch);
+            valueCalculated.getAndIncrement();
+            return EXPECTED_RESULT;
+        });
+
+        new Thread(() -> {
+            assertEquals(EXPECTED_RESULT, supplier.get());
+            threadsCompleteLatch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            assertEquals(EXPECTED_RESULT, supplier.get());
+            threadsCompleteLatch.countDown();
+        }).start();
+
+        releaseValueLatch.countDown();
+
+        await(threadsCompleteLatch);
+
+        assertEquals(1, valueCalculated.get());
+    }
+
+    // TODO Read up on best approach
+    private static void await(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted", e);
+        }
     }
 }
